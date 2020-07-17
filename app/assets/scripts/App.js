3184e041664d316f360561a3ccb31d57392671aa
oguzy187 <oguz.yaman@leine-weber.net>
2020-07-17 18:30:47 +0200

    Finished the git course.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,6 +5,15 @@ import RevealOnScroll from './modules/RevealOnScroll.js'
 import StickyHeader from './modules/StickyHeader.js'
 import ClientArea from './modules/ClientArea.js'
 
+// React Related Code Goes HERE
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import React components that we created
+import MyAmazingComponent from './modules/MyAmazingComponent'
+
+ReactDOM.render(<MyAmazingComponent />, document.querySelector("#my-react-example"));
+
 new ClientArea();
 new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
